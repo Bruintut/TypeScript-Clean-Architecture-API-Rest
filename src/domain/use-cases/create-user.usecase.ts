98143601852490e18bef541745e9b0654d255559
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-28 17:26:21 -0300

    alter export default from export interface

--- src/domain/use-cases/create-user.usecase.ts
+++ src/domain/use-cases/create-user.usecase.ts
@@ -1,6 +1,6 @@
-import UserInputEntity from "../entities/input-data/user-input.entity";
+import { UserInputEntity } from "../entities/input-data/user-input.entity";
 import { UserEntity } from "../entities/user.entity";
 
-export default class ICreateUserUseCase{
-    //execute(data: UserInputEntity): Promise<UserEntity>;
+export interface ICreateUserUseCase{
+    execute(data: UserInputEntity): Promise<UserEntity>;
 }
