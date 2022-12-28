2d7ea92e292bce630e45ff898560cdc7022f7a2d
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-28 17:02:44 -0300

    feat: add function create a user

--- src/data/repositories/user-repository.data.ts
+++ src/data/repositories/user-repository.data.ts
@@ -1,4 +1,7 @@
-export default class IUserRepository{
-    
+import { UserInputData } from "../types/input";
+import { UserOutputData } from "../types/output";
+
+export interface IUserRepository{
+   createUser(data: UserInputData): Promise<UserOutputData>; 
 
 }
---
+++ src/infra/repositories/user.repository.ts
@@ -0,0 +1,6 @@
+/*import IUserRepository from "../../data/repositories/user-repository.data";
+import { UserEntity } from "../../domain/entities/user.entity";
+
+export class UserRepository implements IUserRepository{
+    async createUser(data: UserEntity): Promise<UserEntity>
+}*/
