4e9690665beb91aecb9f517c33a4c99056586518
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-28 14:49:06 -0300

    first commit

---
+++ src/domain/models/User.entity.ts
@@ -0,0 +1,10 @@
+export class UserEntity {
+    public userId?: string ;
+    public username: string;
+    public firstName: string;
+    public lastName: string;
+    public email: string;
+    public password: string;
+
+    
+}
