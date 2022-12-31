b64049ffefab9880309fb446e889871ea892514e
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-31 15:06:32 -0300

    feat: factory makeCreateUser

--- src/data/repositories/user-repository.data.ts
+++ src/data/repositories/user-repository.data.ts
@@ -3,7 +3,7 @@ import { UserOutputData } from "../types/output";
 
 export interface IUserRepository{
    createUser(data: UserInputData): Promise<UserOutputData>;
-   updateUser(data: UserUpdateInputData): Promise<UserOutputData>
+   updateUser(data: UserUpdateInputData, id: string): Promise<UserOutputData>
    findUserById(userId: string): Promise<UserOutputData>;
    findAllUsers(): Promise<UserOutputData[]>;
    deleteUser(userId: string): Promise<UserOutputData> 
