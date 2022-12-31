ed02fbff368b4bbaefbcc54e45bdbf02393c826b
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-12-31 18:13:14 -0300

    pack

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,9 @@
-import { env } from "./adapters/config/env"
+import  {env} from "./adapters/config/env"
 import { MongoHelper } from "../infra/helpers/mongo-helpers";
 
-MongoHelper.connect(env.mongoUrl).then(async()=> {
-    const {setupApp} = await import('./adapters/config/app')
-    const app = await setupApp()
-    app.listen(env.port, ()=> console.log(`Server running at http://localhost:${env.port}`))
-})
+
+    MongoHelper.connect(env.mongoUrl).then(async()=> {
+        const {setupApp} = await import('./adapters/config/app')
+        const app = await setupApp()
+        app.listen(env.port, ()=> console.log(`Server running at http://localhost:${env.port}`))
+    })
